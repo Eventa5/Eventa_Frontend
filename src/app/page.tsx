d19afbe329f8ecb3f4e5aa9c5c252ee53491c969
hsin yu <tina2793778@gmail.com>
2025-04-29 16:45:54 +0800

    style: 💄 新增 Noto Sans TC 字體設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="scroll-m-20 text-4xl font-black tracking-tight lg:text-5xl leading-[1.2]">
+          AI 精準推薦
+          <br />
+          活動票券一手掌握
+        </h1>
         <Image
           className="dark:invert"
           src="/next.svg"
